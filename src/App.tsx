b255fd30fa53ebc07f86cd8bab04b5d499d7e8bf
Davis Weimer <davisweimer@gmail.com>
2024-04-11 11:50:36 -0600

    add todo functions + components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import TodoForm from './components/TodoForm/TodoForm';
+import TodoItem from './components/TodoItem/TodoItem';
+import TodoList from './components/TodoList/TodoList';
 import './App.css';
 
+interface Todo {
+  id: number;
+  text: string;
+  completed: boolean;
+}
+
 function App() {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const addTodo = (text: string) => {
+    const newTodo: Todo = {
+      id: Date.now(),
+      text,
+      completed: false,
+    }
+    setTodos([...todos, newTodo])
+  };
+
+  const toggleTodo = (id: number) => {
+    const updatedTodos = todos.map(todo =>
+      todo.id === id ? {...todo, completed: !todo.completed } : todo
+    );
+    setTodos(updatedTodos);
+  };
+
+  const deleteTodo = (id: number) => {
+    const updatedTodos = todos.filter(todo => todo.id !== id);
+    setTodos(updatedTodos)
+  }
+
   return (
-    <>
-    </>
+    <div className='App'>
+      <h1>Todo List</h1>
+      <TodoForm addTodo={addTodo} />
+      <TodoList />
+    </div>
   );
 }
 
