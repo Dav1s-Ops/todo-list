3253a421c343b7208af3b490f21fc8780a930b73
Davis Weimer <davisweimer@gmail.com>
2024-04-11 11:51:01 -0600

    init commit

---
+++ src/components/TodoItem/TodoItem.tsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import './TodoItem.css'
+
+type Props = {}
+
+type State = {}
+
+export default class TodoItem extends Component<Props, State> {
+  state = {}
+
+  render() {
+    return (
+      <div>TodoItem</div>
+    )
+  }
+}
---
+++ src/components/TodoList/TodoList.tsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import './TodoList.css'
+
+type Props = {}
+
+type State = {}
+
+export default class TodoList extends Component<Props, State> {
+  state = {}
+
+  render() {
+    return (
+      <div>TodoList</div>
+    )
+  }
+}
